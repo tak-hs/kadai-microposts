7fd74173628a3a2272c38ab408e05e3a73461d3a
tak-hs <newgreend@yahoo.co.jp>
2018-11-09 06:58:37 +0000

    favorite

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,5 @@
 class UsersController < ApplicationController
-  before_action :require_user_logged_in, only: [:index, :show, :followings, :followers]
+  before_action :require_user_logged_in, only: [:index, :show, :followings, :followers, :liking]
   
   
   def index
@@ -41,6 +41,13 @@ class UsersController < ApplicationController
     @followers = @user.followers.page(params[:page])
     counts(@user)
   end
+
+
+  def likes
+        @user = User.find(params[:id])
+        @micropost = @user.likings.page(params[:page])
+  end
+
 
 
   private
--- app/models/user.rb
+++ app/models/user.rb
@@ -7,12 +7,18 @@ class User < ApplicationRecord
   has_secure_password
   
   has_many :microposts
+  
   has_many :relationships
   has_many :followings, through: :relationships, source: :follow
   has_many :reverses_of_relationship, class_name: 'Relationship', foreign_key: 'follow_id'
   has_many :followers, through: :reverses_of_relationship, source: :user
 
-  
+  has_many :favorites
+  has_many :likings, through: :favorites, source: :micropost
+  has_many :reverses_of_favorite, class_name: 'Favorite', foreign_key: 'micropost_id'
+  has_many :liked, through: :reverses_of_favorites, source: :user
+
+#follow
   def follow(other_user)
     unless self == other_user
       self.relationships.find_or_create_by(follow_id: other_user.id)
@@ -30,6 +36,21 @@ class User < ApplicationRecord
 
   def feed_microposts
     Micropost.where(user_id: self.following_ids + [self.id])
+  end
+
+
+#likes
+  def like(other_micropost)
+      self.favorites.find_or_create_by(micropost_id: other_micropost.id)
+  end
+
+  def unlike(other_micropost)
+    favorite = self.favorites.find_by(micropost_id: other_micropost.id)
+    favorite.destroy if favorite
+  end
+
+  def liking?(other_micropost)
+    self.likings.include?(other_micropost)
   end
 
 end
